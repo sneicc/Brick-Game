601f499dfc5dc24e07311daa3de48b9d98607c74
Andrey <102552777+sneicc@users.noreply.github.com>
2023-04-03 22:54:18 +0300

    1

---
+++ Assets/scripts/Behaivor/DoublingMod.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoublingMod : MonoBehaviour
+{
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("GameBall"))
+        {
+            gameObject.SetActive(false);
+
+            Vector3 position = GetClonePosition(other.transform.position, other.gameObject.GetComponent<SphereCollider>().radius, other.gameObject.transform.localScale);
+            var clone = Instantiate(other.gameObject, position, other.gameObject.transform.rotation);
+
+            clone.gameObject.GetComponent<Rigidbody>().velocity = other.gameObject.GetComponent<Rigidbody>().velocity;
+            clone.gameObject.GetComponent<BallB>().IsClone = true;     
+        }
+    }
+
+    private Vector3 GetClonePosition(Vector3 originalPosition, float radius, Vector3 scale)
+    {
+        float offset = radius * scale.x * 2 + 0.05f;
+        float trueRadius = radius * scale.x;
+
+        Vector3 left = new Vector3(originalPosition.x - offset, originalPosition.y, originalPosition.z);
+        Vector3 right = new Vector3(originalPosition.x + offset, originalPosition.y, originalPosition.z);
+        Vector3 up = new Vector3(originalPosition.x, originalPosition.y + offset, originalPosition.z);
+        Vector3 down = new Vector3(originalPosition.x, originalPosition.y - offset, originalPosition.z);
+
+        if (!Physics.CheckSphere(left, trueRadius)) return left;
+        else if (!Physics.CheckSphere(right, trueRadius)) return right;
+        else if (!Physics.CheckSphere(down, trueRadius)) return down;
+        else if (!Physics.CheckSphere(up, trueRadius)) return up;
+        else return originalPosition;
+
+    }
+}
---
+++ Assets/scripts/Behaivor/PlatformaMove.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformaMove : MonoBehaviour
+{
+    public Rigidbody Rigidbody { get; private set; }
+
+    public Vector3 Direction { get; private set; }
+    public float Speed = 10;
+
+    private Collider Collider;
+
+    public float MaxBounceAngle = 80;
+
+    void Start()
+    {
+        Rigidbody = GetComponent<Rigidbody>();
+        Direction = Vector3.zero;
+
+        Collider = GetComponent<Collider>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            Direction = Vector3.left;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            Direction = Vector3.right;
+        }
+        else
+        {
+            Direction = Vector3.zero;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Rigidbody.AddForce(Direction * Speed);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("GameBall"))
+        {
+            Vector3 platformPosition = transform.position;
+            Vector3 contactPoint = collision.GetContact(0).point;
+
+            float offset = platformPosition.x - contactPoint.x;
+            float width = Collider.bounds.size.x / 2;
+
+            float currentAngle = Vector3.SignedAngle(Vector3.up, collision.rigidbody.velocity, Vector3.forward);
+            float bounceAngle = (offset / width) * MaxBounceAngle;
+            float newAngle = Mathf.Clamp(currentAngle + bounceAngle, -MaxBounceAngle, MaxBounceAngle);
+
+            Quaternion rotation = Quaternion.AngleAxis(newAngle, Vector3.forward);
+            collision.rigidbody.velocity = rotation * Vector3.up * collision.rigidbody.velocity.magnitude;
+        }
+    }
+
+}
