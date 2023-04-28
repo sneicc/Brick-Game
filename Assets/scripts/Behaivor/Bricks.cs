c863890741e81cbadf3928f800a02191181db814
Andrey <102552777+sneicc@users.noreply.github.com>
2023-04-28 21:32:02 +0300

    Урон от лазера и корректная отрисовка по вертикали
    Заменить Raycast на SphereCast

--- Assets/scripts/Behaivor/Bricks.cs
+++ Assets/scripts/Behaivor/Bricks.cs
@@ -10,6 +10,7 @@ public class Bricks : MonoBehaviour
     public Material[] Materials;
     private Renderer Renderer;
     public ParticleSystem VFX;
+    public ParticleSystem BasicVFX;
 
     public bool Unbreakable;
     public bool Fragile;
@@ -75,7 +76,8 @@ public class Bricks : MonoBehaviour
         if (Fragile || HP <= 0)
         {
             gameObject.SetActive(false);
-            if(collision is not null) ShowVFX(collision);
+            if (collision is not null) ShowVFXBallBrick(collision);
+            else ShowVFXBasic();
         }
         else if (Gradient)
         {
@@ -86,6 +88,13 @@ public class Bricks : MonoBehaviour
         {
             Renderer.material = Materials[HP - 1];
         }
+    }
+
+    private void ShowVFXBasic()
+    {
+        if (BasicVFX is null) return;
+
+        Instantiate(BasicVFX.gameObject, transform.position, transform.rotation);
     }
 
     public void Hit(int Damage)
@@ -102,16 +111,23 @@ public class Bricks : MonoBehaviour
         if (HPText is not null) HPText.text = HP.ToString();
     }
 
-    private void ShowVFX(Collision collision)
+    private void ShowVFXBallBrick(Collision collision)
     {
         if (VFX is null) return;
 
         var shape = VFX.shape;
         //Vector3 rotation = new Vector3(collision.transform.eulerAngles.x, 90, 0);
-        Quaternion quaternion = Quaternion.LookRotation(collision.gameObject.GetComponent<BallB>().PrevVelocity[1]);
-        Vector3 rotation = quaternion.eulerAngles;
-        shape.rotation = rotation;// * -1f;
-        Instantiate(VFX.gameObject, transform.position, transform.rotation);
+        try 
+        {
+            Quaternion quaternion = Quaternion.LookRotation(collision.gameObject.GetComponent<BallB>().PrevVelocity[1]);
+            Vector3 rotation = quaternion.eulerAngles;
+            shape.rotation = rotation;// * -1f;
+            Instantiate(VFX.gameObject, transform.position, transform.rotation);
+        }
+        catch
+        {
+            Debug.Log($"{collision.gameObject.name} не имеет компонент BallB");
+        }       
     }
 
     private float ConvertRange(float oldMinBorder, float oldMaxBorder, float newMinBorder,float newMaxBorder, float value)
--- Assets/scripts/Behaivor/Laser.cs
+++ Assets/scripts/Behaivor/Laser.cs
@@ -2,10 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.VFX;
+using UnityEngineInternal;
 
 public class Laser : MonoBehaviour
 {
     public int WorkTime = 2;
+    public int Dagame = 10;
+
     public bool ShootRight;
     public bool ShootLeft;
     public bool ShootUp;
@@ -22,37 +25,62 @@ public class Laser : MonoBehaviour
     private bool _isActive;
 
     private float _heightOffset;
+    private float _raycastLength = 2000f;
 
     void Start()
     {
-        _heightOffset = transform.position.z - LaserBeamHeightOffset;
+        _heightOffset = transform.position.z - LaserBeamHeightOffset; //офсет высоты эффекта столкновения (чтобы избежать перекрытия эффектов)
 
         Camera mainCamera = Camera.main;
         float cameraHeight = mainCamera.orthographicSize * 2f;
         float cameraWidth = cameraHeight * mainCamera.aspect;
         Vector3 cameraPosition = mainCamera.transform.position;
 
+        int borderMask = LayerMask.GetMask("Border");
+
         _leftBound = cameraPosition.x - cameraWidth / 2f + BoundOffset;
         _rightBound = cameraPosition.x + cameraWidth / 2f - BoundOffset;
-        _bottomBound = cameraPosition.y - mainCamera.orthographicSize + BoundOffset;
-        _topBound = cameraPosition.y + mainCamera.orthographicSize - BoundOffset;
+        _bottomBound = FindBorder(Vector3.down, borderMask).y;
+        _topBound = FindBorder(Vector3.up, borderMask).y;
     }
 
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (_isActive) return;
+        
         if (collision.gameObject.CompareTag("GameBall"))
         {
-            _isActive = true;
-
-            if(ShootRight) ShootLaserRight();
-            if(ShootLeft) ShootLaserLeft();
-            if (ShootUp) ShootLaserUp();
-            if (ShootDown) ShootLaserDown();
-
-            Destroy(gameObject, WorkTime);
+            Activate();
         }
+    }
+
+    public void Activate()
+    {
+        if (_isActive) return;
+        _isActive = true;
+
+        if (ShootRight)
+        {
+            ShootLaserRight();
+            MakeDamage(transform.right);
+        }
+        if (ShootLeft)
+        {
+            ShootLaserLeft();
+            MakeDamage(-transform.right);
+        }
+        if (ShootUp)
+        {
+            ShootLaserUp();
+            MakeDamage(transform.up);
+        }
+        if (ShootDown)
+        {
+            ShootLaserDown();
+            MakeDamage(-transform.up);
+        }
+
+        Destroy(gameObject, WorkTime);
     }
 
     private void ShootLaserRight()
@@ -97,5 +125,33 @@ public class Laser : MonoBehaviour
 
         laser.transform.parent = transform;
         ContactPoint.transform.parent = transform;
+    }
+
+    private Vector3 FindBorder(Vector3 direction, int layerMask)
+    {
+
+        RaycastHit hit;
+        Physics.Raycast(transform.position, transform.TransformDirection(direction), out hit, _raycastLength, layerMask);
+
+        return hit.point;
+
+    }
+
+    private void MakeDamage(Vector3 direction)
+    {
+        RaycastHit[] hits;
+        hits = Physics.RaycastAll(transform.position, direction, _raycastLength);
+
+        foreach (var hit in hits)
+        {
+            if (hit.collider.CompareTag("Brick"))
+            {
+                hit.collider.GetComponent<Bricks>().Hit(Dagame);
+            }
+            else if (hit.collider.CompareTag("Laser"))
+            {
+                hit.collider.GetComponent<Laser>().Activate();
+            }
+        }
     }
 }
